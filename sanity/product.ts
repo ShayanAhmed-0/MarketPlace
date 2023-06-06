6559373947cc2646446cd47103eb3077660fb49e
Shayan Ahmed <shayanlost1@gmail.com>
2023-06-07 04:38:03 +0500

    initial

---
+++ sanity/product.ts
@@ -0,0 +1,22 @@
+export const product = {
+  name: "product",
+  type: "document",
+  title: "Product",
+  fields: [
+    {
+      name: "title",
+      titel: "Title",
+      type: "string",
+    },
+    {
+        name: "description",
+        titel: "product Description",
+        type: "string",
+    },
+    {
+        name: "image",
+        titel: "product Image",
+        type: "image",
+    },
+  ],
+};
