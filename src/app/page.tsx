c31b96cde48a3897115c396dc33747804f24d9ed
Shayan Ahmed <shayanlost1@gmail.com>
2023-07-03 01:14:29 +0500

    UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,48 +2,19 @@ import Image from 'next/image'
 import {client} from "@/lib/sanityClient" 
 import { Image as IImage} from 'sanity'
 import { urlForImage } from '../../sanity/lib/image'
-import ProductCard from './ProductCard';
+import ProductCard from '../components/ProductCard';
+import Carousel from '@/view/Carousel';
+import Hero from '@/view/Hero';
+import Promotions from '@/view/Promotions';
 
-export const getProductData=async()=>{
-  const res = await client.fetch(
-    `*[_type=="product"]{
-      price,
-      _id,
-      description,
-      title,
-      image,
-      category -> {
-        name
-      }
-    }`
-  );
-  return res;
-};
-
-interface IProduct {
-  price: number;
-  _id: string;
-  description: string;
-  title: string;
-  image: IImage;
-  category: {
-    name: string;
-  };
-}
 
 export default async function Home() {
-  
-  const data:IProduct[]=await getProductData()
-console.log(data)
+// console.log(data)
   return (
    <>
-   <div className='grid justify-center grid-cols-[auto,auto,auto] gap-10'>
-      {data.map((item) => (
-       <div>
-        <ProductCard item={item}/>
-       </div>
-      ))}
-      </div>
+   <Hero/>
+   <Promotions/>
+  <Carousel/>
    </>
   )
 }
