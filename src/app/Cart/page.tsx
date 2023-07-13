53590a87c4d63bc40a472a22be8a7253a7085755
Shayan Ahmed <shayanlost1@gmail.com>
2023-07-13 18:47:25 +0500

    fix

--- src/app/Cart/page.tsx
+++ src/app/Cart/page.tsx
@@ -1,20 +1,12 @@
 import React from 'react'
-import { cookies } from "next/headers"
 
-const page = async() => {
-
-    // const getCart=async()=>{
-    //     const res = await fetch("http://localhost:3000/api/cart",{
-    //         method: "GET"
-    //     })
-    // }
-    const user=cookies().get("user_id")?.value as string
-    console.log(user)
+const page = () => {
   return (
     <div>
-      {user}
+      Cart
     </div>
   )
 }
 
 export default page
+
--- src/app/SignIn/page.tsx
+++ src/app/SignIn/page.tsx
@@ -3,7 +3,6 @@ import { SignIn } from "@clerk/nextjs";
 const page=()=> {
   return (
     <div className="absolute items-center justify-center bg-red-800">
-    SHayan
   <SignIn/>
   </div>
     )
